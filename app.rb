be5e3c61a0b3374ef4c770ae3238e5a5cd7b5073
Saturday Academy <satacad@cat1.local>
2013-05-04 12:11:00 -0700

    adding stuff

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'koala'
 
 get '/' do
   erb :index
@@ -8,8 +9,8 @@ end
 get '/login' do
   @username = params[:username]
   @password = params[:password]
-  if @username == 'Aleeo34152'
-    if @password == 'iloveruchi'
+  if @username == 'ginny'
+    if @password == 'jeong'
       redirect "/name?name=Alex&planet=Earth"
     else
       @error = "Wrong Password"
@@ -29,4 +30,20 @@ get '/name' do
   else
     erb :name
   end
+end
+
+get '/secrets' do
+  return redirect '/' unless session [:user]
+  @graph = Koala::Facebook :: API.new('BAACEdEose0cBAOXBUpW07cdH4nnCqdckBalqjliAZCkHsetJnPhOY2Kosd66911OK0Rn2Q7myzzlZA9lGnxjWg9uUdYzDeZCPqWohp7qPavxrmueacDKv5LcgCVRI3ZC7w8ykYSOpljph0AUWACU1xDsjOhetsgpTb6QJ3VQZBqUkbePr6ck2EzdR5izZB1H1yTlfaHZCcZCvZARUq03SShlWZATkCAGbfi4C9FmYNyFsI0QZDZD')
+  @picture = @graph.get_picture('jpozelc')
+  erb :secret
+end
+
+post '/go_here' do
+  redirect "http://#{params[:place]}.com"
+end
+
+get '/logout' do
+  sessions[:user] = nil
+  redierct '/'
 end
